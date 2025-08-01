08085a153a73bf851c4db946680f44b253a3ef07
Loja <Loja@host.docker.internal>
2025-08-01 14:39:28 -0300

    2025 07 16 - Trabalhando com cidades

--- Projeto1/src/main/webapp/js/funcoes.js
+++ Projeto1/src/main/webapp/js/funcoes.js
@@ -7,3 +7,14 @@ function validarSenha(input){
     
 }
 
+function formata_mascara(campo_passado,mascara){
+    var campo = campo_passado.value.lenght;
+    var saida = mascara.substring(0,1);
+    var texto = mascara.substring(campo);
+    if(texto.substring(0,1) !== saida){
+        campo_passado.value += texto.substring(0,1);
+        
+    }
+    
+}
+
